e61099cef39241e619872cb2485a8aa6691a1aa4
pocz.mate <mate.pocz@gmail.com>
2023-04-13 22:42:47 +0200

    Add BucketURL

---
+++ src/main/java/org/reduct/common/BucketURL.java
@@ -0,0 +1,15 @@
+package org.reduct.common;
+
+public enum BucketURL {
+      CREATE_BUCKET("api/v1/b/%s");
+
+      private final String url;
+
+      BucketURL(String url) {
+         this.url = url;
+      }
+
+      public String getUrl() {
+         return url;
+      }
+}

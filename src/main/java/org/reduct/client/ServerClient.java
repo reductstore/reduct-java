e124730b6a7bb028074c93cad91063a628a77f6c
pocz.mate <mate.pocz@gmail.com>
2023-04-15 21:34:06 +0200

    ServerClient uses x-reduct-error header value

--- src/main/java/org/reduct/client/ServerClient.java
+++ src/main/java/org/reduct/client/ServerClient.java
@@ -1,5 +1,7 @@
 package org.reduct.client;
 
+import org.reduct.common.exception.ReductException;
+import org.reduct.common.exception.ReductSDKException;
 import org.reduct.model.server.ServerInfo;
 
 public interface ServerClient {
@@ -7,8 +9,12 @@ public interface ServerClient {
    /**
     * Attempts to retrieve the server information. Such as, version, bucket count, usage, uptime, oldest record,
     * latest record, and default settings.
-    * Returns {@link org.reduct.model.server.ServerInfo} object if the request is successful.
-    * Throws {@link org.reduct.common.exception.ReductException} if the request fails.
+    *
+    * @return {@link ServerInfo} object if the request is successful.
+    * @throws ReductException    if the request fails. The instance of the exception holds
+    *                            the error message returned in the x-reduct-error header and the
+    *                            status code to indicate the failure.
+    * @throws ReductSDKException If, any client side error occurs.
     */
-   ServerInfo getServerInfo();
+   ServerInfo getServerInfo() throws ReductException, ReductSDKException;
 }

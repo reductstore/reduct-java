47275ee57cf8eb9d2fe3e4aa1f2c52442b7d5339
Галиханов Рустам Дамирович <r.galikhanov@bftcom.com>
2023-12-17 21:52:26 +0500

    Added list request for getting collection of buckets. Reffactoring, fix tests

--- src/main/java/org/reduct/client/ReductServerClient.java
+++ src/main/java/org/reduct/client/ReductServerClient.java
@@ -2,10 +2,12 @@ package org.reduct.client;
 
 import com.fasterxml.jackson.core.JacksonException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Getter;
 import org.reduct.client.config.ServerProperties;
 import org.reduct.common.ServerURL;
 import org.reduct.common.exception.ReductException;
 import org.reduct.common.exception.ReductSDKException;
+import org.reduct.model.bucket.Buckets;
 import org.reduct.model.server.ServerInfo;
 
 import java.io.IOException;
@@ -13,10 +15,18 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.List;
 
-public class ReductServerClient extends ReductClient implements ServerClient {
+import static org.reduct.utils.Strings.isNotBlank;
+
+@Getter
+public class ReductServerClient implements ServerClient {
 
    private static final String REDUCT_ERROR_HEADER = "x-reduct-error";
+   protected final ServerProperties serverProperties;
+   protected final HttpClient httpClient;
+   protected final ObjectMapper objectMapper;
+   protected final String token;
 
    /**
     * Constructs a new ReductServerClient with the given properties.
@@ -39,43 +49,81 @@ public class ReductServerClient extends ReductClient implements ServerClient {
       this(serverProperties, HttpClient.newHttpClient(), accessToken);
    }
 
-   ReductServerClient(ServerProperties serverProperties, HttpClient client, String accessToken) {
-      super(serverProperties, client, new ObjectMapper(), accessToken);
+   ReductServerClient(ServerProperties serverProperties, HttpClient httpClient, String accessToken) {
+      this.httpClient = httpClient;
+      this.serverProperties = serverProperties;
+      this.objectMapper = new ObjectMapper();
+      this.token = accessToken;
    }
 
    @Override
    public ServerInfo getServerInfo() throws ReductException, ReductSDKException {
-      URI serverInfoUri = URI.create("%s/%s".formatted(serverProperties.getBaseUrl(), ServerURL.SERVER_INFO.getUrl()));
-      HttpRequest getRequest = HttpRequest.newBuilder()
+      URI serverInfoUri = URI.create("%s/%s".formatted(getServerProperties().getBaseUrl(), ServerURL.SERVER_INFO.getUrl()));
+      HttpRequest.Builder builder = HttpRequest.newBuilder()
               .GET()
-              .uri(serverInfoUri)
-              .header("Authorization", "Bearer %s".formatted(token))
-              .build();
-      HttpResponse<String> httpResponse = sendRequest(getRequest);
-      if (httpResponse.statusCode() == 200) {
-         return parseServerInfo(httpResponse.body());
-      } else {
-         String errorMessage = httpResponse.headers()
-                 .firstValue(REDUCT_ERROR_HEADER)
-                 .orElse("Failed to create bucket");
-         throw new ReductException(errorMessage, httpResponse.statusCode());
+              .uri(serverInfoUri);
+      if(isNotBlank(getToken())) {
+         builder.header("Authorization", "Bearer %s".formatted(getToken()));
       }
-   }
-
-   private HttpResponse<String> sendRequest(HttpRequest getRequest) {
       try {
-         return httpClient.send(getRequest, HttpResponse.BodyHandlers.ofString());
-      } catch (IOException e) {
+         HttpResponse<String> httpResponse = getHttpClient().send(builder.build(), HttpResponse.BodyHandlers.ofString());
+         if (httpResponse.statusCode() == 200) {
+            return parseObject(httpResponse.body(), ServerInfo.class);
+
+         } else {
+            String errorMessage = httpResponse.headers()
+                    .firstValue(REDUCT_ERROR_HEADER)
+                    .orElse("Failed to create bucket");
+            throw new ReductException(errorMessage, httpResponse.statusCode());
+         }
+      }
+      catch (IOException e) {
          throw new ReductSDKException("An error occurred while processing the request", e);
-      } catch (InterruptedException e) {
+      }
+      catch (InterruptedException e) {
          Thread.currentThread().interrupt();
          throw new ReductSDKException("Thread has been interrupted while processing the request", e);
       }
    }
 
-   private ServerInfo parseServerInfo(String serverInfo) {
+   @Override
+   public Buckets getList() throws ReductException, ReductSDKException {
+      URI serverInfoUri = URI.create("%s/%s".formatted(getServerProperties().getBaseUrl(), ServerURL.LIST.getUrl()));
+      HttpRequest.Builder builder = HttpRequest.newBuilder()
+              .GET()
+              .uri(serverInfoUri);
+      if(isNotBlank(getToken())) {
+         builder.header("Authorization", "Bearer %s".formatted(getToken()));
+      }
       try {
-         return objectMapper.readValue(serverInfo, ServerInfo.class);
+         HttpResponse<String> httpResponse = getHttpClient().send(builder.build(), HttpResponse.BodyHandlers.ofString());
+         if (httpResponse.statusCode() == 200) {
+             return parseObject(httpResponse.body(), Buckets.class);
+         } else {
+            String errorMessage = httpResponse.headers()
+                    .firstValue(REDUCT_ERROR_HEADER)
+                    .orElse("Failed to create bucket");
+            throw new ReductException(errorMessage, httpResponse.statusCode());
+         }
+      }
+      catch (IOException e) {
+         throw new ReductSDKException("An error occurred while processing the request", e);
+      }
+      catch (InterruptedException e) {
+         Thread.currentThread().interrupt();
+         throw new ReductSDKException("Thread has been interrupted while processing the request", e);
+      }
+   }
+   private <T> T parseObject(String serverInfo, Class<T> tClass) {
+      try {
+         return objectMapper.readValue(serverInfo, tClass);
+      } catch (JacksonException e) {
+         throw new ReductSDKException("The server returned a malformed response.");
+      }
+   }
+   private <T> List<T> parseObjectAsList(String serverInfo, Class<T> tClass) {
+      try {
+         return objectMapper.readValue(serverInfo, objectMapper.getTypeFactory().constructCollectionType(List.class, tClass));
       } catch (JacksonException e) {
          throw new ReductSDKException("The server returned a malformed response.");
       }
---
+++ src/main/java/org/reduct/model/bucket/Bucket.java
@@ -0,0 +1,49 @@
+package org.reduct.model.bucket;
+
+import com.fasterxml.jackson.annotation.JacksonAnnotation;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+import java.math.BigInteger;
+
+@Getter
+@NoArgsConstructor
+@EqualsAndHashCode
+@ToString
+@Builder
+@AllArgsConstructor
+public class Bucket {
+    /**
+     * Name of the bucket
+     */
+    @JsonProperty("name")
+    private String name;
+    /**
+     * Number of entries in the bucket
+     */
+    @JsonProperty("entry_count")
+    private Integer entryCount;
+    /**
+     * Size of stored data in the bucket in bytes
+     */
+    @JsonProperty("size")
+    private Integer size;
+    /**
+     * Unix timestamp of oldest record in microseconds
+     */
+    @JsonProperty("oldest_record")
+    private BigInteger oldestRecord;
+    /**
+     * Unix timestamp of latest record in microseconds
+     */
+    @JsonProperty("latest_record")
+    private BigInteger latestRecord;
+
+    /**
+     *
+     */
+    @JsonProperty("is_provisioned")
+    private Boolean isProvisioned;
+
+}
